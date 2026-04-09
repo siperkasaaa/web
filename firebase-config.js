9c7c410d57668beb1bf042a04ae37a0bf2533d99
siperkasaaa <siperkasaaa@gmail.com>
2026-04-10 06:34:31 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Konfigurasi Firebase dari project monitoring-ow
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCP34EbQnelN7z7lpR-eVHj9BnuK0RgGjY",
+  authDomain: "monitoring-ow.firebaseapp.com",
+  projectId: "monitoring-ow",
+  storageBucket: "monitoring-ow.firebasestorage.app",
+  messagingSenderId: "197133986327",
+  appId: "1:197133986327:web:be61cdaba3d2c55f580a52",
+  measurementId: "G-G0QFDEFEMG",
+};
