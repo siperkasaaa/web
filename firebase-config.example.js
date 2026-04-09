d137649a10ebdf5988995f3677fb4d3139a91acf
siperkasaaa <siperkasaaa@gmail.com>
2026-04-10 06:33:44 +0700

    Create firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+// Copy file ini menjadi `firebase-config.js`, lalu isi nilai dari Firebase Console.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIza...",
+  authDomain: "nama-project.firebaseapp.com",
+  projectId: "nama-project",
+  storageBucket: "nama-project.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456",
+};
